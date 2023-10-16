f8963d1d534ceaa3c40adfda1e94e38d724f2de1
Mendo94822 <58573852+Mendo94822@users.noreply.github.com>
2023-10-16 11:50:49 -0500

    Finished Main Website Function

--- index.js
+++ index.js
@@ -5,17 +5,41 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true })); 
+
+let userInput = '';
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    try {
+        const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${userInput}`);
+        const result = response.data.data;
+        console.log(result)
+        const animeData = result.map(anime => ({
+            title: anime.title,
+            titleJP: anime.title_japanese,
+            status: anime.status,
+            airing: anime.airing,
+            aired: anime.aired.string,
+            epLength: anime.duration,
+            rating: anime.rating,
+            score: anime.score,
+            rank: anime.rank,
+            synopsis: anime.synopsis,
+            background: anime.background,
+            season: anime.season,
+            year: anime.year,
+        }));
+        res.render("index.ejs", { animeData });
+    } catch(error) {
+        console.log("error");
+        res.status(500)
+    }
 });
 
 app.post("/input", (req, res) => {
-    userInput = req.body.search; 
-    res.redirect("/");
-});
+    userInput = req.body.search;
+    res.redirect("/")
+})
 
 app.listen(port, () => {
-    console.log(`Server is Running on Port ${port}`);
+    console.log(`Server is Running on Port ${port}`)
 });
